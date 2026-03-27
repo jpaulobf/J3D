cb76408172f321319873a5ac6ea020b214263870
jpaulobf <jpaulobf@gmail.com>
2026-03-27 21:30:14 +0000

    Cleaning and refinaments

--- src/j3d/factory/RenderFactory.java
+++ src/j3d/factory/RenderFactory.java
@@ -3,7 +3,17 @@ package j3d.factory;
 import j3d.render.IRenderer;
 import j3d.enums.RenderType;
 
+/**
+ * Create a Renderer based on the type.
+ */
 public class RenderFactory {
+    /**
+     * Create a Renderer based on the type.
+     * @param type
+     * @param width
+     * @param height
+     * @return
+     */
     public static IRenderer createRenderer(RenderType type, int width, int height) {
         if (RenderType.OPENGL == type) {
             return new j3d.render.OpenGLRenderer(width, height);
